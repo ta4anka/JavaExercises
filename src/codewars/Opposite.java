38b3fa06cda2505c41d8a70b4eec5de5ce1c8ec9
ta4anka <vladymirsheyko@gmail.com>
2019-01-01 20:32:33 +0200

    find opposite number

---
+++ src/codewars/Opposite.java
@@ -0,0 +1,17 @@
+package codewars;
+/*Very simple, given a number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?*/
+
+public class Opposite {
+    public static int opposite(int number) {
+        return number * -1;
+    }
+}
+
+
+
+
